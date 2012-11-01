dbd1ec41aa4d5f117ba490668f9fbb9361ecdd8d
dohoangkhiem <dohoangkhiem@gmail.com>
2012-11-01 12:04:11 +0700

    Add file

--- twitter/twitter-streaming-demo/src/main/java/khiem/twitter/demo/TwitterStreaming.java
+++ twitter/twitter-streaming-demo/src/main/java/khiem/twitter/demo/TwitterStreaming.java
@@ -4,10 +4,8 @@ package khiem.twitter.demo;
  * Hello world!
  *
  */
-public class TwitterStreaming
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class TwitterStreaming {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
