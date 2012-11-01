49bcd9129d86f3d970d652d3f548fbb6c2716777
dohoangkhiem <dohoangkhiem@gmail.com>
2012-11-01 11:36:48 +0700

    init project

---
+++ twitter/twitter-streaming-demo/src/main/java/khiem/twitter/demo/TwitterStreaming.java
@@ -0,0 +1,13 @@
+package khiem.twitter.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class TwitterStreaming
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
